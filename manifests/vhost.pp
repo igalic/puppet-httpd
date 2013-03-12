ad7066b8305a508957ebce53ca5b11bdaf926a64
Igor Galić <i.galic@brainsware.org>
2013-03-12 13:56:23 +0000

    make httpd::vhost's $title semantic
    I don't want $instance => to repeat over every vhost, so I'm making the
    $title of httpd::vhost semantic: A split occurs by |.
    Also, document the change.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,26 +1,49 @@
-define httpd::vhost ( $domain = $title,
-  $instance,
-  $order,
-  $remote_port         = '80',
-  $remote_proto        = 'http',
+define httpd::vhost ( 
+  $port                = '80',
+  $proto               = 'http',
   $type                = 'static',
+  $order               = '',
   $conf_vhost_template = $httpd::params::conf_vhost_template,
   $vhostconf           = $httpd::params::vhostconf,
 ){
   include 'httpd::params'
   include 'concat::setup'
 
-  unless ($order >= 100 and $order <= 90000) {
+  validate_re ($title, '[|]')
+  validate_re ($proto, 'https?')
+
+  $domain = split($title, '[|]')
+  $subdomain = $domain[0]
+  $instance  = $domain[1]
+
+
+
+  if $order == '' {
+    $order_real = 100
+  } else {
+    $order_real = $order
+  }
+  unless ($order_real >= 100 and $order_real <= 90000) {
     fail ("Please supply a valid \$order. It must be between 100 and 9000.")
   }
 
-  $subdomain = regsubst($domain, "(.*).${instance}$", '\1')
+  if $proto == 'https' {
+    $remote_proto  = $proto
+    if $port == '80' {
+      $remote_port = 443
+    } else {
+      $remote_port = $port
+    }
+  } else {
+    $remote_proto = $proto
+    $remote_port  = $port
+  }
 
   # creates the configuration (fragment)
   $configfile = "${vhostconf}/${instance}/httpd.conf"
   concat::fragment { "${configfile}_$subdomain":
     target  => $configfile,
     content => template($conf_vhost_template),
-    order   => $order,
+    order   => $order_real,
   }
 }
